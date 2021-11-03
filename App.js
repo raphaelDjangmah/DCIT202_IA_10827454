f98c91815a3c33de112efbe68e21098a738cf02e
raphaelDjangmah <92959036+raphaelDjangmah@users.noreply.github.com>
2021-11-03 11:01:38 +0000

    WELCOME SCREEN DESIGN

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import WelcomeScreen from './screens/WelcomeScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    // SCREENS TO BE USED DURING THE PROJECT
+    Welcome: WelcomeScreen
   },
-});
+  
+  {
+    initialRouteName: 'Welcome',
+
+    defaultNavigationOptions: {
+      title: 'LapShop',
+    },
+  }
+);
+
+export default createAppContainer(navigator);
