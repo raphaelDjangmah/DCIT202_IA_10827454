5f3116bd447b93c73b494918744ab11a79395fe3
raphaelDjangmah <rdosro-agbo@st.ug.edu.gh>
2021-11-17 03:08:47 +0000

    Data That will be passed to the app

---
+++ constants/dummyData.js
@@ -0,0 +1,251 @@
+import images from '../assets/images/images';
+
+export const results = [
+
+    // APPLE PRODUCTS
+   
+    {
+        id: "0002",
+        colorway: "White",
+        model: "MACBOOK",
+        name: "MacBook Air 13 (2021), M1, 8GB, 256GB, 256GB, Plata",
+        brand: "Macbook Air",
+        releaseDate: 2020,
+        retailPrice: 2999,
+        story: "Astonishing breakthrough in MacBook, The Pro: 2020 - Apple MacBook Air 13, M1, 8GB, 256GB, Plata Released in 2020",
+        image: {
+            original: images.mac2,
+            small: images.mac2,
+            thumbnail: images.mac2,
+        }
+    },
+    {
+        id: "0005",
+        colorway: "White",
+        model: "HP",
+        name: "Newest HP 15.6, TouchScreen 1135G7",
+        brand: "",
+        releaseDate: 2021,
+        retailPrice: 599,
+        story: "New on HP. 11th Generation all the way with quality specs",
+        image: {
+            original: images.hp2,
+            small: images.hp2,
+            thumbnail: images.hp2,
+        }
+    },
+
+  
+
+    {
+        id: "0006",
+        colorway: "Cream",
+        model: "HP",
+        name: "HP Gorgeous new Spectra",
+        brand: "",
+        releaseDate: 2020,
+        retailPrice: 359,
+        story: "HP AGAIN!! 15-inch features 4K OLED screen options x 360",
+        image: {
+            original: images.hp3,
+            small: images.hp3,
+            thumbnail: images.hp3,
+        }
+    },
+
+    // HP PRODUCTS
+    
+
+    {
+        id: "0002",
+        colorway: "Grey",
+        model: "MACBOOK",
+        name: "Eye 13 retina - 512GB SSD - 8 GB RAM 2015 Gris",
+        brand: "Macbook Pro",
+        releaseDate: 2020,
+        retailPrice: 3999,
+        story: "General type de produit macbook retina 13 reconditionne date de sortie 2015 systeme d exploitation mac os processeur core i5 - 2 7 tbz memoire et stockage disque dur 312 GB SSD RAM 7 GB Carte...",
+        image: {
+            original: images.mac3,
+            small: images.mac3,
+            thumbnail: images.mac3,
+        }
+    },
+
+
+    {
+        id: "0001",
+        colorway: "Black",
+        model: "MACBOOK",
+        name: "Macbook Pro-19  Retina 13 - 1TB SSD - 12GB RAM 2017 Gris",
+        brand: "",
+        releaseDate: 2019,
+        retailPrice: 1099,
+        story: "Tech Specs for MacBook Pro 2019 - Techable Dates sold, processor, type, memory info, hard drive, prices and more",
+        image: {
+            original: images.mac1,
+            small: images.mac1,
+            thumbnail: images.mac1,
+        }
+    },
+    
+
+    {
+        id: "0008",
+        colorway: "Grey",
+        model: "DELL",
+        name: "Dell XPS 13 New Notebook",
+        brand: "",
+        releaseDate: 2020,
+        retailPrice: 499,
+        story: "DELL is here UHD+TouchScreen (WLD 3840 x 2400), 512 SSD i7",
+        image: {
+            original: images.dell2,
+            small: images.dell2,
+            thumbnail: images.dell2,
+        }
+    },
+
+
+    {
+        id: "0011",
+        colorway: "Black",
+        model: "ACER",
+        name: "Acer Aspire 7 A715-73G 75BW",
+        brand: "",
+        releaseDate: 2020,
+        retailPrice: 399,
+        story: "Acer is here to surprise you with the new Aspire",
+        image: {
+            original: images.acer2,
+            small: images.acer2,
+            thumbnail: images.acer2,
+        }
+    },
+    // DELL PRODUCTS
+    {
+        id: "0007",
+        colorway: "White",
+        model: "DELL",
+        name: "Dell XP 13",
+        brand: "",
+        releaseDate: 2020,
+        retailPrice: 499,
+        story: "Late-2020 fell the greatness and refinery of the finest Dell on the Map",
+        image: {
+            original: images.dell1,
+            small: images.dell1,
+            thumbnail: images.dell1,
+        }
+    }, {
+        id: "0004",
+        colorway: "Black",
+        model: "HP",
+        name: "HP Elite DragonFly 13.3, TouchScreen 2 in 1 Notebook - 1920 x 1080",
+        brand: "",
+        releaseDate: 2019,
+        retailPrice: 499,
+        story: "Breakthrough in hp tech : intel core i5, 8th Gen 8GB RAM, 256SSD Quad-core",
+        image: {
+            original: images.hp1,
+            small: images.hp1,
+            thumbnail: images.hp1,
+        }
+    },
+
+    {
+        id: "0009",
+        colorway: "Silver",
+        model: "DELL",
+        name: "Dell 14(2021) Inspiring 5410",
+        brand: "",
+        releaseDate: 2021,
+        retailPrice: 699,
+        story: "Dell 14  touchscreen laptop, 256GB SSD, Enduring silver colorway MSO Backlit KB + FPR, FHD Display",
+        image: {
+            original: images.dell3,
+            small: images.dell3,
+            thumbnail: images.dell3,
+        }
+    },
+
+   
+
+    // ACER PRODUCTS
+    {
+        id: "0010",
+        colorway: "White",
+        model: "ACER",
+        name: "Acer Swift3",
+        brand: "",
+        releaseDate: 2019,
+        retailPrice: 1099,
+        story: "Intel Core i5 13.5 8GB 512GB SSD",
+        image: {
+            original: images.acer1,
+            small: images.acer1,
+            thumbnail: images.acer1,
+        }
+    },
+    {
+        id: "0012",
+        colorway: "Silver",
+        model: "ACER",
+        name: "Acer Swift 3 laptop Line",
+        brand: "",
+        releaseDate: 2020,
+        retailPrice: 599,
+        story: "Acer Lets get it on with two new ultraslim models",
+        image: {
+            original: images.acer3,
+            small: images.acer3,
+            thumbnail: images.acer3,
+        }
+    },
+
+
+    // SAMSUNG PRODUCTS
+    {
+        id: "0013",
+        colorway: "Pink",
+        model: "SAMSUNG",
+        name: "Samsung Galaxy Book S 2 Ultra",
+        brand: "",
+        releaseDate: 2019,
+        retailPrice: 599,
+        story: "Do you know about the LTE cipset so you can work anywhere, Samsung got you covered",
+        image: {
+            original: images.samsung2,
+            small: images.samsung2,
+            thumbnail: images.samsung2,
+        }
+    }, {
+        id: "0014",
+        colorway: "Black",
+        model: "SAMSUNG",
+        name: "Samsung Galaxy Book Flex 2",
+        brand: "",
+        releaseDate: 2019,
+        retailPrice: 599,
+        story: "Are you ready for the future 2 in 1 pc released by Samsung",
+        image: {
+            original: images.samsung2,
+            small: images.samsung2,
+            thumbnail: images.samsung2,
+        }
+    }, {
+        id: "0015",
+        colorway: "White",
+        model: "SAMSUNG",
+        name: "Samsung Galaxy Book Ion Magnesium",
+        brand: "",
+        releaseDate: 2019,
+        retailPrice: 599,
+        story: "The only laptop that weighs less than a kilo. Wow",
+        image: {
+            original: images.samsung3,
+            small: images.samsung3,
+            thumbnail: images.samsung3,
+        }
+    },
+]
---
+++ constants/theme.js
@@ -0,0 +1,206 @@
+import { Dimensions } from "react-native";
+import { color } from "react-native-reanimated";
+const { width, height } = Dimensions.get("window");
+
+export const COLORS = {
+    // base colors
+    primary: "#111015", 
+    secondary: "#FFFFFF",   
+    tetiary: "#F1F5F8",
+
+    // colors
+    black: "#1E1F20",
+    white: "#FFFFFF",
+    lemon: '#EFFD5F',
+    mustard: '#E8B828',
+    gold   : '#F9A602',
+    fire   : '#FD50F',
+    butter:  '#FABD02',
+    lightGray: "#F5F5F6",
+    lightGray2: "#F6F6F7",
+    lightGray3: "#EFEFF1",
+    lightGray4: "#F8F8F9",
+    transparent: "transparent",
+    darkgray: '#898C95',
+
+    col1 : '#040406',
+    col2 : '#FD9743',
+    col3 : '#58585A',
+    col4 : '#522F19',
+    col5 : '#AA5F28',
+    col6 : '#62462C',
+    col7 : '#BIAF9A',
+    col8 : '#E9A73E'
+};
+
+export const SIZES = {
+    // global sizes
+    base: 8,
+    font: 14,
+    radius: 10,
+    padding: 10,
+    padding2: 12,
+    paddingLarge: 50,
+
+    // font sizes
+    largeTitle: 50,
+    h1: 35,
+    h2: 30,
+    h3: 22,
+    h4: 20,
+    h5: 18,
+    body1: 30,
+    body2: 20,
+    body3: 16,
+    body4: 14,
+    body5: 12,
+
+    // app dimensions
+    width,
+    height
+};
+
+export const HEIGHT = {
+    sm: 50,
+    m: 80,
+    l: 250,
+    xl: height * 0.5,
+    h: height
+}
+
+export const WIDTH = {
+    xs: width * 0.3,
+    sm: width * 0.4,
+    m: width * 0.7,
+    // m: 300,
+    l: "100%",
+}
+
+export const BUTTONS = {
+    primary: {
+        height: HEIGHT.sm,
+        backgroundColor: COLORS.primary,
+        // marginVertical: SIZES.padding,
+        borderRadius: SIZES.radius,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    secondary: {
+        height: HEIGHT.sm,
+        backgroundColor: COLORS.secondary,
+        marginVertical: SIZES.padding,
+        borderRadius: SIZES.radius,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: COLORS.primary,
+        borderWidth: 2.5,
+
+    },
+
+    warning: {
+        height: HEIGHT.sm,
+        backgroundColor: COLORS.secondary,
+        marginVertical: SIZES.padding,
+        borderRadius: SIZES.radius,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: COLORS.gold,
+        borderWidth: 2.5,
+
+    },
+
+    loginBut:{
+        height: HEIGHT.sm,
+        marginVertical: SIZES.padding,
+        borderRadius: SIZES.radius,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLORS.gold,
+        borderColor:COLORS.primary,
+        borderWidth: 2.5,
+    },
+
+    prim1: {
+        backgroundColor: COLORS.col3,
+        borderRadius: SIZES.radius,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 2.5,
+        borderColor: COLORS.col3
+    },
+
+    prim2: {
+        height: HEIGHT.sm,
+        marginVertical: SIZES.padding,
+        borderRadius: SIZES.radius,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLORS.col6,
+        borderColor:COLORS.secondary,
+        borderWidth: 2,
+    }
+}
+
+export const FONTS = {
+    largeTitle: {
+        // fontFamily: "Roboto-regular", 
+        fontSize: SIZES.largeTitle, lineHeight: 55
+    },
+    h1: {
+        // fontFamily: "Roboto-Black", 
+        fontSize: SIZES.h1, lineHeight: 36
+    },
+    h2: {
+        // fontFamily: "Roboto-Bold", 
+        fontSize: SIZES.h2, lineHeight: 30
+    },
+    h3: {
+        // fontFamily: "Roboto-Bold", 
+        fontSize: SIZES.h3, lineHeight: 22
+    },
+    h4: {
+        // fontFamily: "Roboto-Bold", 
+        fontSize: SIZES.h4, lineHeight: 22
+    },
+    h5: {
+        // fontFamily: "Roboto-Black", 
+        fontSize: SIZES.h5, lineHeight: 20
+    },
+    body1: {
+        // fontFamily: "Roboto-Regular", 
+        fontSize: SIZES.body1, lineHeight: 36
+    },
+    body2: {
+        // fontFamily: "Roboto-Regular", 
+        fontSize: SIZES.body2, lineHeight: 30
+    },
+    body3: {
+        // fontFamily: "Roboto-Regular", 
+        fontSize: SIZES.body3, lineHeight: 22
+    },
+    body4: {
+        // fontFamily: "Roboto-Regular", 
+        fontSize: SIZES.body4, lineHeight: 22
+    },
+    body5: {
+        // fontFamily: "Roboto-Regular", 
+        fontSize: SIZES.body5, lineHeight: 22
+    },
+    primary: {
+        fontSize: SIZES.body3,
+        lineHeight: 30,
+        color: COLORS.tetiary,
+        fontWeight: "500",
+    },
+    secondary: {
+        fontSize: SIZES.body3,
+        lineHeight: 30,
+        color: COLORS.black,
+        fontWeight: "500"
+    },
+};
+
+const appTheme = { COLORS, SIZES, FONTS };
+
+export default appTheme;
