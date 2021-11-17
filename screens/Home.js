dd49e9ff6e477a0b480a6906f684812830b54fa4
raphaelDjangmah <rdosro-agbo@st.ug.edu.gh>
2021-11-17 02:51:04 +0000

    Home Screen Adding Other Screens

--- screens/Home.js
+++ screens/Home.js
@@ -1,23 +1,21 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity, ImageBackground, FlatList } from 'react-native';
 import { COLORS, SIZES } from '../constants/theme';
-import axios from "axios";
-import { GenderContext } from '../src/Components/Context';
 import MainCategory from '../src/Components/MainCategory';
-import { results } from '../constants/dummyData';
 import RenderExplore from '../src/Components/RenderExplore';
-import RenderLatest from '../src/Components/RenderLatest';
 
 
 function Home({ navigation }) {
     
 
     return (
-        <ScrollView style={styles.container} >
+        <View style={{backgroundColor: 'lightgray'}}>
+            <ScrollView style={styles.container} >
             <MainCategory />
             <RenderExplore navigation={navigation} />
-            <RenderLatest navigation={navigation} />
-        </ScrollView>
+           </ScrollView>
+        </View>
+        
     )
 }
 
@@ -25,7 +23,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingVertical: SIZES.paddingLarge,
-        backgroundColor: COLORS.tetiary,
+
+        // backgroundColor: COLORS.tetiary,
         // paddingBottom: SIZES.padding * 10
     }
 })
